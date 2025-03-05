661f6925ee7c2d56379b97fa99612ac4ad894ddb
Gilbert Johnson <johnson220898@gmail.com>
2025-03-05 23:52:03 +0530

    Text change

--- ArcGIS-JS-React-Samples/react-samples/src/pages/ScenePage.jsx
+++ ArcGIS-JS-React-Samples/react-samples/src/pages/ScenePage.jsx
@@ -2,7 +2,7 @@ import SceneView from "../components/SceneView/SceneView"
 const ScenePage = () => {
   return (
     <>
-        <h3 className='h3-sceneview'>SceneView</h3>
+        <h3 className='h3-sceneview'>SceneView Example</h3>
         <SceneView />
     </>
   )
