b4123c7bd69be97bd6b3a9c25f47596bdfb5fc2f
Gilbert Johnson <johnson220898@gmail.com>
2025-03-06 23:48:13 +0530

    Update home card style

--- ArcGIS-JS-React-Samples/react-samples/src/pages/Home.jsx
+++ ArcGIS-JS-React-Samples/react-samples/src/pages/Home.jsx
@@ -4,30 +4,32 @@ const Home = () => {
   return (
     <>
         <div>React components based Samples</div>
-        <li>
-            <Link to={'/mapview'} >Mapview</Link>
-        </li>
-        <li>
-            <Link to={'/sceneview'}>SceneView</Link>
-        </li>
-        <li>
-            <Link to={'/tilelayer'} >Tile Layer</Link>
-        </li>
-        <li>
-          <Link to={'/widgetbmt'} >Widget - BasemapToggle</Link>
-        </li>
-        <li>
-          <Link to={'/graphic'} >Graphics</Link>
-        </li>
-        <li>
-          <Link to={'/popup'} >Popup with locator</Link>
-        </li>
-        <li>
-          <Link to={'/expand'} >Expand Widget</Link>
-        </li>
-        <li>
-          <Link to={'/popuptemplate'} >Popup Template</Link>
-        </li>
+        <ul className='container'>
+          <li>
+              <Link to={'/mapview'} >Mapview</Link>
+          </li>
+          <li>
+              <Link to={'/sceneview'}>SceneView</Link>
+          </li>
+          <li>
+              <Link to={'/tilelayer'} >Tile Layer</Link>
+          </li>
+          <li>
+            <Link to={'/widgetbmt'} >Widget - BasemapToggle</Link>
+          </li>
+          <li>
+            <Link to={'/graphic'} >Graphics</Link>
+          </li>
+          <li>
+            <Link to={'/popup'} >Popup with locator</Link>
+          </li>
+          <li>
+            <Link to={'/expand'} >Expand Widget</Link>
+          </li>
+          <li>
+            <Link to={'/popuptemplate'} >Popup Template</Link>
+          </li>
+        </ul>
     </>
     
   )
